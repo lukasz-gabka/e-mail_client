91e25cef80abe02f911434c7ed4139282f84a394
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2020-10-30 19:48:08 +0100

    built login window

---
+++ src/project/view/LoginWindowController.java
@@ -0,0 +1,23 @@
+package project.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void loginButtonAction() {
+        
+    }
+}
