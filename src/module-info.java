ce24dbe5fe8be89ffe131cc44ab7b985cfa41524
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2020-11-03 22:14:58 +0100

    added JavaMail and login functionality

--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,8 @@ module e.mail.client {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.web;
+    requires activation;
+    requires java.mail;
 
     opens project;
     opens project.controller;
