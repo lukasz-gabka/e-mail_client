b1d93b512d31bb5e1fbbf077cc4a441b16a49999
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2020-11-06 23:33:07 +0100

    upgraded sorting by size in main window TableView

--- src/project/controller/MainWindowController.java
+++ src/project/controller/MainWindowController.java
@@ -12,6 +12,7 @@ import javafx.util.Callback;
 import project.EmailManager;
 import project.model.EmailMessage;
 import project.model.EmailTreeItem;
+import project.model.SizeInteger;
 import project.view.ViewFactory;
 
 import java.net.URL;
@@ -40,7 +41,7 @@ public class MainWindowController extends BaseController implements Initializabl
     private TableColumn<EmailMessage, String> recipientCol;
 
     @FXML
-    private TableColumn<EmailMessage, Integer> sizeCol;
+    private TableColumn<EmailMessage, SizeInteger> sizeCol;
 
     @FXML
     private TableColumn<EmailMessage, Date> dateCol;
@@ -100,7 +101,7 @@ public class MainWindowController extends BaseController implements Initializabl
         senderCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("sender"));
         subjectCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("subject"));
         recipientCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, String>("recipient"));
-        sizeCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Integer>("size"));
+        sizeCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, SizeInteger>("size"));
         dateCol.setCellValueFactory(new PropertyValueFactory<EmailMessage, Date>("date"));
     }
 
