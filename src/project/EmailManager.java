e24131d622f2977b3e54b5d28317f04fcaa820fb
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2020-11-08 21:30:48 +0100

    added marking messages as read functionality

--- src/project/EmailManager.java
+++ src/project/EmailManager.java
@@ -4,13 +4,18 @@ import javafx.scene.control.TreeItem;
 import project.controller.services.FetchFoldersService;
 import project.controller.services.FolderUpdaterService;
 import project.model.EmailAccount;
+import project.model.EmailMessage;
 import project.model.EmailTreeItem;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
 
     private FolderUpdaterService folderUpdaterService;
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
@@ -35,5 +40,31 @@ public class EmailManager {
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
+    }
+
+    public EmailMessage getSelectedMessage() {
+        return selectedMessage;
+    }
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public EmailTreeItem<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
+
+    public void setRead() {
+        try {
+            selectedMessage.setIsRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessagesCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
