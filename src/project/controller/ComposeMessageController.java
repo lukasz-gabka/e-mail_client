a9451397ac94af60c328311ab57d78faee0315c8
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2020-11-10 21:03:14 +0100

    added compose-message-window

---
+++ src/project/controller/ComposeMessageController.java
@@ -0,0 +1,33 @@
+package project.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
+import project.EmailManager;
+import project.view.ViewFactory;
+
+public class ComposeMessageController extends BaseController {
+
+    @FXML
+    private TextField recipientTextField;
+
+    @FXML
+    private TextField subjectTextField;
+
+    @FXML
+    private HTMLEditor htmlEditor;
+
+    @FXML
+    private Label errorLabel;
+
+    public ComposeMessageController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+
+    @FXML
+    void sendButtonAction() {
+        System.out.println(htmlEditor.getHtmlText());
+        System.out.println("wysyłanie wiadomości");
+    }
+}
